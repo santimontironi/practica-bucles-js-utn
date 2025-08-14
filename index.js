bb002846fc7be47ea62582826c287af60756e147
santimontironi <santiimontironi@gmail.com>
2025-08-14 19:05:36 -0300

    function notes agregada

---
+++ index.js
@@ -0,0 +1,35 @@
+function notes(){
+    let acumulador = 0
+    let flag = true
+
+    for (let index = 0; index < 3; index++) {
+
+        let nota = Number(prompt(`Ingrese la nota ${index + 1}:`))
+        flag = true
+
+        while(flag){
+
+            if (isNaN(nota)){
+                console.log("Nota ingresada no válida")
+                nota = Number(prompt(`Ingrese la nota ${index + 1} nuevamente:`))
+            }else if(nota == ''){
+                console.log("Debe de ingresar un numero")
+                nota = Number(prompt(`Ingrese la nota ${index + 1} nuevamente:`))
+            }else if(nota < 0 && nota > 10){
+                console.log("Debe de ingresar una nota entre 0 y 10")
+                nota = Number(prompt(`Ingrese la nota ${index + 1} nuevamente:`))
+            }else{
+                acumulador += nota
+                flag = false
+            }
+
+        }
+        
+    }
+
+    const promedio = acumulador/3
+    console.log("El promedio de las notas es: ",promedio)
+
+}
+
+notes()
