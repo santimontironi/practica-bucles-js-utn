22d37ac7240b26317ba9465a97c5ef07231b2696
santimontironi <santiimontironi@gmail.com>
2025-08-14 19:26:26 -0300

    mejoras

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 function notes(){
     let acumulador = 0
-    let flag = true
 
     for (let index = 0; index < 3; index++) {
 
         let nota = Number(prompt(`Ingrese la nota ${index + 1}:`))
-        flag = true
-
-        while(flag){
+        
+        while(true){
 
             if (isNaN(nota)){
                 console.log("Nota ingresada no válida")
@@ -15,12 +13,12 @@ function notes(){
             }else if(nota == ''){
                 console.log("Debe de ingresar un numero")
                 nota = Number(prompt(`Ingrese la nota ${index + 1} nuevamente:`))
-            }else if(nota < 0 && nota > 10){
+            }else if(nota < 0 || nota > 10){
                 console.log("Debe de ingresar una nota entre 0 y 10")
                 nota = Number(prompt(`Ingrese la nota ${index + 1} nuevamente:`))
             }else{
                 acumulador += nota
-                flag = false
+                break;
             }
 
         }
